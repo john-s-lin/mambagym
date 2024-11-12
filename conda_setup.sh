88faf5157e036e5326a16062dd09ac653ef4f5dc
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2024-11-12 17:39:53 -0500

    patch: create target dirs for sizing if they don't exist

--- conda_setup.sh
+++ conda_setup.sh
@@ -10,12 +10,28 @@
 #SBATCH --partition=gpunodes
 #SBATCH --gres=gpu:rtx_4090:1
 
+TARGET_DIR_PREFIX="/w/383/${USER}"
+
 mkdir -p logs
+
+if [ ! -d "${TARGET_DIR_PREFIX}/conda_envs" ]; then
+    mkdir -p "${TARGET_DIR_PREFIX}/conda_envs"
+    conda config --add envs_dirs "${TARGET_DIR_PREFIX}/conda_envs"
+else
+    echo "${TARGET_DIR_PREFIX}/conda_envs already exists"
+fi
+
+if [ ! -d "${TARGET_DIR_PREFIX}/conda_pkgs" ]; then
+    mkdir -p "${TARGET_DIR_PREFIX}/conda_pkgs"
+    conda config --add pkgs_dirs "${TARGET_DIR_PREFIX}/conda_pkgs"
+else
+    echo "${TARGET_DIR_PREFIX}/conda_pkgs already exists"
+fi
 
 if conda info --envs | grep -q mambagym; then
     echo "mambagym already exists"
 else
-    conda env create -f environment.yml -p "/w/383/${USER}/conda_envs/mambagym" -v -y
+    conda env create -f environment.yml -p "${TARGET_DIR_PREFIX}/${USER}/conda_envs/mambagym" -v -y
 fi
 
 exit 0
