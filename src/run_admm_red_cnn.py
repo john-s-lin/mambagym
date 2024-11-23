889607c9df5f2357e4744eb7aefbc81425cf14b9
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2024-11-23 12:37:47 -0500

    rebase: main

--- src/run_admm_red_cnn.py
+++ src/run_admm_red_cnn.py
@@ -1,12 +1,18 @@
+from datetime import datetime
+
 import dival
 import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import Subset
+
 
 # TODO: set dival config location to /w/383/johnlin/.dival/
-# TODO: set dival lodopab_dataset to /w/331/yukthiw/tmp/lodopab
 # Ref: https://jleuschn.github.io/docs.dival/dival.config.html
 def main():
     dataset = dival.datasets.get_standard_dataset("lodopab")
-    print(dataset)
+    data = dataset.create_torch_dataset("train")
+    data = Subset(data, indices=range(200))
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
 
 if __name__ == "__main__":
