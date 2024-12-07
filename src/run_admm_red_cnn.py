437c2db775195aacb1be073b5ca2e2016d11d329
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2024-12-06 19:32:35 -0500

    feat: add plots for the test set

--- src/run_admm_red_cnn.py
+++ src/run_admm_red_cnn.py
@@ -105,7 +105,7 @@ def main():
     torch.manual_seed(0)
 
     dataset = dival.datasets.get_standard_dataset("lodopab")
-    data = dataset.create_torch_dataset("train")
+    data = dataset.create_torch_dataset("test")
     data = Subset(data, indices=range(1000))
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
